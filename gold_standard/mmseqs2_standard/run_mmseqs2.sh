716b1286f609497337cc21d3d4704deab7bbec46
shafferm <michael.shaffer@ucdenver.edu>
2021-09-28 14:38:59 -0600

    Add filtering of fastas with bbtools reformat.sh

--- gold_standard/mmseqs2_standard/run_mmseqs2.sh
+++ gold_standard/mmseqs2_standard/run_mmseqs2.sh
@@ -9,13 +9,14 @@ conda config --add channels bioconda
 conda config --add channels conda-forge
 conda info -a
 
-conda install wget pandas mmseqs2
+conda install wget pandas mmseqs2 bbmap
 
 # get data sheet
 wget https://raw.githubusercontent.com/NCBI-Codeathons/psss-datasets/master/data.tsv
 
 # script filters data down to files with S3 paths for all references and query
-
+# TODO: update path to main repo or switch to cloning repo instead of pulling specific file
+wget https://raw.githubusercontent.com/shafferm/psss-team2/main/gold_standard/mmseqs2_standard/filter_data_to_marine_contigs.py
 python filter_data_to_marine_contigs.py
 
 # download all data
@@ -23,11 +24,14 @@ mkdir query
 while read query_path; do
     aws s3 cp $query_path query
 done < query_paths.txt
+cat query/*.fna > query/query.fna
+reformat.sh in=query/query.fna out=query/query.ml500.fna ml=500
 
 mkdir reference
 while read reference_path; do
-    aws s3 cp s3://psss-metagenomics-codeathon-data/marine/$reference_path/assembly/$reference_path_contigs.fna reference
+    aws s3 cp $reference_path reference
 done < reference_paths.txt
 cat reference/*.fna > reference/reference.fna
+reformat.sh in=reference/reference.fna out=reference/reference.ml500.fna ml=500
 
 # run mmseqs2
